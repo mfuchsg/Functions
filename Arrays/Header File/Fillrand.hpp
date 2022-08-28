f99b571803a1a05bc67f709907c6ab1c5467e092
mfuchsg <mfoxg@icloud.com>
2022-08-29 00:14:02 +0400

    done

---
+++ Arrays/Header File/Fillrand.hpp
@@ -0,0 +1,16 @@
+//
+//  Fillrand.hpp
+//  Arrays
+//
+//  Created by Matvei Gavrilchik on 28.08.22.
+//
+
+#include "stdafx.hpp"
+#include <stdio.h>
+
+void fillrand(int arr[], const int n);
+void fillrand(double arr[], const int n);
+void fillrand(short arr[], const int n);
+void fillrand(float arr[], const int n);
+void fillrand(char arr[], const int n);
+void fillrand(int arr[ROWS][COLS], const int ROWS, const int COLS);
